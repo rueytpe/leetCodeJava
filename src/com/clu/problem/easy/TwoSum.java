3279972b3136e7586e51d3992f6bde295227cfd2
“rueytpe” <rueytpe@gmail.com>
2023-02-01 08:53:19 +0800

    Update TwoSum

--- src/com/clu/problem/easy/TwoSum.java
+++ src/com/clu/problem/easy/TwoSum.java
@@ -1,9 +1,13 @@
 package com.clu.problem.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.clu.util.ArrayUtils;
 
 /**
  * Problem 1
+ * Two Sum (Easy)
  * 
  * Given an array of integers nums and an integer target, return indices of the
  * two numbers such that they add up to target.
@@ -29,6 +33,26 @@ public class TwoSum {
 	}
 
 	public int[] twoSum(int[] nums, int target) {
+		int[] result = null;
+		if (nums == null || nums.length <= 1) {
+			return result;
+		}
+		Map<Integer, Integer> numMap = new HashMap<>();
+		numMap.put(nums[0],0);
+		for (int i = 1; i < nums.length; i++) {
+			Integer firstNumber = numMap.get(target - nums[i]);
+			if (firstNumber == null) {
+				numMap.put(nums[i], i);
+				continue;
+			} else {
+				result = new int[]{firstNumber, i};
+				return result;
+			}
+		}
+		return result;
+	}
+	
+	public int[] twoSum_v1(int[] nums, int target) {
 		if (nums == null || nums.length <= 1) {
 			return null;
 		}
